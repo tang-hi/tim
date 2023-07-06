beafd15e6c5cdc7bb369b2abd372203fb9a9dfe2
tang donghai <tangdhcs@gmail.com>
2023-07-06 17:20:47 +0800

    update tim's config

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-vim.cmd.colorscheme "catppuccin-latte"
-require "user.cmp"
-require "user.lsp"
+require "options"
+require "keymaps"
+require "Lazy"
+require "autocommands"
+
---
+++ lua/utils/init.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.servers = {
+  "lua_ls",
+  "clangd",
+  "cssls",
+  "html",
+  "tsserver",
+  "pyright",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "jdtls",
+}
+
+return M
